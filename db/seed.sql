36b9cbc0e878b63b0e4cbd91b6df5a292451ca65
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-02-04 15:13:47 -0500

    updated the seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,19 @@
 \c bathroom_app;
 
-INSERT INTO reviews (rating, comments)
+-- INSERT INTO bathroom (bathroom_id)
+-- VALUES
+-- (1),
+-- (2),
+-- (3),
+-- (4),
+-- (5),
+-- (6);
+
+INSERT INTO reviews (rating, comments, bathroom_id)
 VALUES
-  (4, 'Very clean and well-maintained.'),
-  (3, 'Average facilities, could use improvement.'),
-  (4, 'Beautiful surroundings, clean restroom.'),
-  (2, 'Convenient location, but the restroom is not kept clean'),
-  (5, 'Peaceful atmosphere, top-notch cleanliness.'),
-  (1, 'Very diry');
+  (4, 'Very clean and well-maintained.', 1),
+  (3, 'Average facilities, could use improvement.', 2),
+  (4, 'Beautiful surroundings, clean restroom.', 3),
+  (2, 'Convenient location, but the restroom is not kept clean', 4),
+  (5, 'Peaceful atmosphere, top-notch cleanliness.', 5),
+  (1, 'Very dirty', 6);
