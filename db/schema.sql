0b88a8d5c1df13d0c27ae3ec60eba38370e4ff8e
Natyka Callwood <Natykacallwood@pursuit.org>
2024-02-04 12:09:17 -0500

    Created the schema for the reviewers, added the reviewer_id to the bathroomreviews table.

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,17 @@ CREATE DATABASE bathroom_app;
 
 \c bathroom_app;
 
+CREATE TABLE reviewers (
+    reviewer_id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT
+);
+
 CREATE TABLE bathroomreviews (
     review_id SERIAL PRIMARY KEY,
     bathroom_name VARCHAR(255) NOT NULL,
     bathroom_address VARCHAR(255) NOT NULL,
     description TEXT,
-    rating INTEGER NOT NULL
+    rating INTEGER NOT NULL,
+    reviewer_id INTEGER REFERENCES reviewers(reviewer_id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -9,10 +9,10 @@ VALUES
     ("Tyrell", "Oliver"),
     ("Yulonda", "Wyatt");
 
-INSERT INTO bathroomreviews (bathroom_name, bathroom_address, rating, description)
+INSERT INTO bathroomreviews (bathroom_name, bathroom_address, rating, description, reviewer_id)
 VALUES
-  ('Kuku Korean Cuisine bathroom', '12-09 Jackson Ave, Queens, NY 11101', 4, 'Very clean and well-maintained.'),
-  ('Cozy Corner Bathroom', '10-37 Jackson Ave, Long Island City, NY 11101', 3, 'Average facilities, could use improvement.'),
-  ('Natures Haven Bathroom', '27-10 43rd Ave, Queens, NY 11101', 4, 'Beautiful surroundings, clean restroom.'),
-  ('Feather Factory Hotel Bathroom', 'Queens County, 26-32 Skillman Ave, Long Island City, NY 11101', 2, 'Good amenities, convenient location.'),
-  ('CVS Bathroom', '2501 Jackson Ave, Long Island City, NY 11101', 5, 'Peaceful atmosphere, top-notch cleanliness.');
+  ('Kuku Korean Cuisine bathroom', '12-09 Jackson Ave, Queens, NY 11101', 4, 'Very clean and well-maintained.', 1),
+  ('Cozy Corner Bathroom', '10-37 Jackson Ave, Long Island City, NY 11101', 3, 'Average facilities, could use improvement.', 2),
+  ('Natures Haven Bathroom', '27-10 43rd Ave, Queens, NY 11101', 4, 'Beautiful surroundings, clean restroom.', 3),
+  ('Feather Factory Hotel Bathroom', 'Queens County, 26-32 Skillman Ave, Long Island City, NY 11101', 2, 'Good amenities, convenient location.', 4),
+  ('CVS Bathroom', '2501 Jackson Ave, Long Island City, NY 11101', 5, 'Peaceful atmosphere, top-notch cleanliness.', 5);
