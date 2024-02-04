08bbc4194e820c6935e83353428d3c97725b4776
Natyka Callwood <Natykacallwood@pursuit.org>
2024-02-04 11:19:46 -0500

    Fixed the name of the table to all lowercase and remove a semicolon from the seed.sql file.

--- db/seed.sql
+++ db/seed.sql
@@ -6,7 +6,7 @@ VALUES
   ('Cozy Corner Bathroom', '456 Comfort Lane, Townsville, Countrytopia', 3, 'Average facilities, could use improvement.'),
   ('Natures Haven Bathroom', '789 Nature Avenue, Green City, Natureshire', 4, 'Beautiful surroundings, clean restroom.'),
   ('Cityscape Rest Stop', '101 Urban Road, Metroville, Citytopolis', 2, 'Good amenities, convenient location.'),
-  ('Serene Oasis Toilets', '202 Peaceful Park, Tranquil Town, Sereneland', 5, 'Peaceful atmosphere, top-notch cleanliness.');
+  ('Serene Oasis Toilets', '202 Peaceful Park, Tranquil Town, Sereneland', 5, 'Peaceful atmosphere, top-notch cleanliness.'),
   ('Shell Gas Station', '410 Unicorn Road, Narnia, Countyville', 1, 'Not clean would not return!!'),
   ('Hooville Bathroom', '111 Hooville Street, Hooville, Suesland', 5, 'Comfortable and clean.'),
   ('Milea Bathroom Station', '303 Venture Avenue, Blue City, Adventuretown', 2, 'Needs to be updated, rough toilet paper.'),
