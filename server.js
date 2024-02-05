b1fa84c7b60c5c5a45e0a0aae65a3f7ee75aa988
anickacodes <anickalewis@pursuit.org>
2024-02-05 18:41:18 -0500

    single server

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const app = require("./app");
-require("dotenv").config();
+// const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = require('./app.js');
+const PORT2 = 3001; // Choose any available port
+// Define your Google Maps API endpoint
+const BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+// Define your Google Maps API key
+require("dotenv").config()
+const API_KEY = process.env.API_KEY;
 
-const PORT1 = process.env.PORT1;
-
-app.listen(PORT1, () => {
-  console.log(`The server is running on port: ${PORT1}`);
+app.use(cors())
+// Set up a route to proxy requests to the Google Maps API
+app.get('/places', async (req, res) => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        ...req.query // Forward query parameters from the client to the Google Maps API
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response).json(error.response.data);
+  }
+});
+// Start the Express server
+app.listen(PORT2, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT2}`);
 });
